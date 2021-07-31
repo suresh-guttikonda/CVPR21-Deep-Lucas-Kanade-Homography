097f9972fb7c4ef1f68ecf7569251afd0afe52e7
guttikonda-suresh <guttikondasuresh7@gmail.com>
2021-07-31 19:39:23 +0200

    code changes to train level two

--- model_ours/train_level_two.py
+++ model_ours/train_level_two.py
@@ -6,9 +6,9 @@ from data_read import *
 from net import *
 import matplotlib.pyplot as plt
 import numpy as np
-
+import tensorflow as tf
 import argparse
-
+import random
 import os
 
 
@@ -37,28 +37,33 @@ parser.add_argument('--epoch_start', action="store", dest="epoch_start", type=in
 parser.add_argument('--sample_noise', action="store", dest="sample_noise", type=int, default=4,help='samples noise number, 4 for Google, 2 for MSCOCO')
 
 
-parser.add_argument('--lambda_loss', action="store", dest="lambda_loss", type=int, default=0.2,help='0.2 for Google, 0.1 for MSCOCO')
+parser.add_argument('--lambda_loss', action="store", dest="lambda_loss", type=float, default=0.2,help='0.2 for Google, 0.1 for MSCOCO')
 
 
 parser.add_argument('--epoch_num', action="store", dest="epoch_num", type=int, default=10,help='how many epochs to train')
 
 
 input_parameters = parser.parse_args()
+input_parameters.seed = 42
 
 
-
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-  # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
-  try:
-    tf.config.experimental.set_virtual_device_configuration(
-        gpus[0],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4500)])
-    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-  except RuntimeError as e:
-    # Virtual devices must be set before GPUs have been initialized
-    print(e)
+random.seed(input_parameters.seed)
+np.random.seed(input_parameters.seed)
+tf.random.set_seed(input_parameters.seed)
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# if gpus:
+#   # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+#   try:
+#     tf.config.experimental.set_virtual_device_configuration(
+#         gpus[0],
+#         [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4000)])
+#     logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+#     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+#   except RuntimeError as e:
+#     # Virtual devices must be set before GPUs have been initialized
+#     print(e)
 
 
 if input_parameters.feature_map_type=='regular':
@@ -118,12 +123,12 @@ def initial_motion_COCO():
     # prepare source and target four points
     matrix_list=[]
     for i in range(input_parameters.batch_size):
-       
+
         src_points=[[0,0],[127,0],[127,127],[0,127]]
 
         tgt_points=[[32,32],[160,32],[160,160],[32,160]]
 
-    
+
         src_points=np.reshape(src_points,[4,1,2])
         tgt_points=np.reshape(tgt_points,[4,1,2])
 
@@ -136,7 +141,7 @@ def initial_motion_COCO():
 def construct_matrix(initial_matrix,scale_factor,batch_size):
     #scale_factor size_now/(size to get matrix)
     initial_matrix=tf.cast(initial_matrix,dtype=tf.float32)
-    
+
     scale_matrix=np.eye(3)*scale_factor
     scale_matrix[2,2]=1.0
     scale_matrix=tf.cast(scale_matrix,dtype=tf.float32)
@@ -154,29 +159,29 @@ def construct_matrix(initial_matrix,scale_factor,batch_size):
 
 
 def average_cornner_error(batch_size,predicted_matrix,u_list,v_list,top_left_u=0,top_left_v=0,bottom_right_u=127,bottom_right_v=127):
-    
+
     four_conner=[[top_left_u,top_left_v,1],[bottom_right_u,top_left_v,1],[bottom_right_u,bottom_right_v,1],[top_left_u,bottom_right_v,1]]
     four_conner=np.asarray(four_conner)
     four_conner=np.transpose(four_conner)
     four_conner=np.expand_dims(four_conner,axis=0)
     four_conner=np.tile(four_conner,[batch_size,1,1]).astype(np.float32)
-    
+
     new_four_points=tf.matmul(predicted_matrix,four_conner)
-    
+
     new_four_points_scale=new_four_points[:,2:,:]
     new_four_points= new_four_points/new_four_points_scale
-    
-    
+
+
     u_predict=new_four_points[:,0,:]
     v_predict=new_four_points[:,1,:]
-    
+
     average_conner=tf.math.pow(u_predict-u_list,2)+tf.math.pow(v_predict-v_list,2)
     #print (np.shape(average_conner))
     average_conner=tf.reduce_sum(average_conner)/batch_size
-    
-    
+
+
     return average_conner
-    
+
 
 
 '''
@@ -184,10 +189,10 @@ def compute_ssim(img_1,img_2):
     C1 = 0.01 ** 2
     C2 = 0.03 ** 2
     paddings = tf.constant([[0,0],[1, 1,], [1, 1],[0,0]])
-        
+
     img_1=tf.pad(img_1, paddings, "REFLECT")
     img_2=tf.pad(img_2, paddings, "REFLECT")
-        
+
     mu_1=tf.nn.avg_pool2d(img_1,ksize=3,strides=1,padding='VALID')
     mu_2=tf.nn.avg_pool2d(img_2,ksize=3,strides=1,padding="VALID")
 
@@ -212,7 +217,7 @@ def gt_motion_rs(u_list,v_list,batch_size=1):
     # prepare source and target four points
     matrix_list=[]
     for i in range(batch_size):
-       
+
         src_points=[[0,0],[127,0],[127,127],[0,127]]
 
         #tgt_points=[[32*2+1,32*2+1],[160*2+1,32*2+1],[160*2+1,160*2+1],[32*2+1,160*2+1]]
@@ -220,7 +225,7 @@ def gt_motion_rs(u_list,v_list,batch_size=1):
         tgt_points=np.concatenate([u_list[i:(i+1),:],v_list[i:(i+1),:]],axis=0)
         tgt_points=np.transpose(tgt_points)
         tgt_points=np.expand_dims(tgt_points,axis=1)
-       
+
         src_points=np.reshape(src_points,[4,1,2])
         tgt_points=np.reshape(tgt_points,[4,1,2])
 
@@ -234,7 +239,7 @@ def gt_motion_rs_random_noisy(u_list,v_list,batch_size,lambda_noisy):
     # prepare source and target four points
     matrix_list=[]
     for i in range(batch_size):
-       
+
         src_points=[[0,0],[127,0],[127,127],[0,127]]
 
         #tgt_points=[[32*2+1,32*2+1],[160*2+1,32*2+1],[160*2+1,160*2+1],[32*2+1,160*2+1]]
@@ -242,7 +247,7 @@ def gt_motion_rs_random_noisy(u_list,v_list,batch_size,lambda_noisy):
         tgt_points=np.concatenate([u_list[i:(i+1),:],v_list[i:(i+1),:]],axis=0)
         tgt_points=np.transpose(tgt_points)
         tgt_points=np.expand_dims(tgt_points,axis=1)
-       
+
         src_points=np.reshape(src_points,[4,1,2])
         tgt_points=np.reshape(tgt_points,[4,1,2])
 
@@ -251,9 +256,9 @@ def gt_motion_rs_random_noisy(u_list,v_list,batch_size,lambda_noisy):
         element_h_matrix=np.reshape(h_matrix,(9,1))
         noisy_matrix=np.zeros((9,1))
         for jj in range(8):
-            #if jj!=0 and jj!=4: 
-            noisy_matrix[jj,0]=element_h_matrix[jj,0]*lambda_noisy[jj]  
-        noisy_matrix=np.reshape(noisy_matrix,(3,3))    
+            #if jj!=0 and jj!=4:
+            noisy_matrix[jj,0]=element_h_matrix[jj,0]*lambda_noisy[jj]
+        noisy_matrix=np.reshape(noisy_matrix,(3,3))
         matrix_list.append(noisy_matrix)
     return np.asarray(matrix_list).astype(np.float32)
 '''
@@ -281,24 +286,25 @@ def calculate_feature_map(input_tensor):
     path_extracted=path_extracted-path_extracted_mean
     path_extracted_transpose=tf.transpose(path_extracted,(0,1,2,4,3))
     variance_matrix=tf.matmul(path_extracted_transpose,path_extracted)
-    
+
     tracevalue=tf.linalg.trace(variance_matrix)
     row_sum=tf.reduce_sum(variance_matrix,axis=-1)
     max_row_sum=tf.math.reduce_max(row_sum,axis=-1)
     min_row_sum=tf.math.reduce_min(row_sum,axis=-1)
     mimic_ratio=(max_row_sum+min_row_sum)/2.0/tracevalue
-    
+
     return  tf.expand_dims(mimic_ratio,axis=-1)
 
 
-    
+
 initial_matrix=initial_motion_COCO()
-    
+
 LK_layer_two=Lucas_Kanade_layer(batch_size=input_parameters.batch_size,height_template=64,width_template=64,num_channels=1)
 
-
-
-
+# Logging
+summaries_flush_secs=10
+summary_writer = tf.compat.v2.summary.create_file_writer(
+    './log/level_two', flush_millis=summaries_flush_secs * 1000)
 
 
 #initial_matrix_scaled=construct_matrix(initial_matrix,scale_factor=0.125,batch_size=input_parameters.batch_size)
@@ -314,15 +320,16 @@ for current_epoch in range(input_parameters.epoch_num):
         data_loader_caller=data_loader_GoogleMap('train')
 
     if input_parameters.dataset_name=='GoogleEarth':
-        data_loader_caller=data_loader_GoogleEarth('train')
+        # data_loader_caller=data_loader_GoogleEarth('train')
+        data_loader_caller=data_loader_Akagi('train')
 
     if input_parameters.dataset_name=='DayNight':
         data_loader_caller=data_loader_DayNight('train')
-        
+
     optimizer = tf.keras.optimizers.Adam(lr=lr,beta_1=0.9)
 
     print("Starting epoch " + str(current_epoch+input_parameters.epoch_start))
-    print("Learning rate is " + str(lr)) 
+    print("Learning rate is " + str(lr))
 
     error_ave_1000=0.0
     convex_loss_total=0.0
@@ -332,13 +339,14 @@ for current_epoch in range(input_parameters.epoch_num):
         input_img,u_list,v_list,template_img=data_loader_caller.data_read_batch(batch_size=input_parameters.batch_size)
 
         if len(np.shape(input_img))<2:
-          level_two_input.save_weights(save_path +'epoch_'+str(input_parameters.epoch_start+current_epoch)+"input_full")
-          level_two_template.save_weights(save_path +'epoch_'+str(input_parameters.epoch_start+current_epoch)+"template_full")
-          break
+            if current_epoch%input_parameters.save_eval_f==0:
+                level_two_input.save_weights(save_path +'epoch_'+str(input_parameters.epoch_start+current_epoch)+"input_full")
+                level_two_template.save_weights(save_path +'epoch_'+str(input_parameters.epoch_start+current_epoch)+"template_full")
+            break
 
         input_feature_one=level_one_input.call(input_img,training=False)
         template_feature_one=level_one_template.call(template_img,training=False)
-  
+
 
         gt_matrix=gt_motion_rs(u_list,v_list,batch_size=input_parameters.batch_size)
         gt_matrix_two=construct_matrix(gt_matrix,scale_factor=0.5,batch_size=input_parameters.batch_size)
@@ -364,27 +372,27 @@ for current_epoch in range(input_parameters.epoch_num):
 
             ssim_middle=ssim_middle_two
 
-            
+
 
             #print ('!!!!!!!!!')
 
             for nn in range(input_parameters.sample_noise):
                 lambda_two=(np.random.rand(8)-0.5)/6.0
                 for mm in range(len(lambda_two)):
-                  if lambda_two[mm]>0 and lambda_two[mm]<0.02:
-                    lambda_two[mm]=0.02
-                  if lambda_two[mm]<0 and lambda_two[mm]>-0.02:
-                    lambda_two[mm]=-0.02
-              
-                
+                    if lambda_two[mm]>0 and lambda_two[mm]<0.02:
+                        lambda_two[mm]=0.02
+                    if lambda_two[mm]<0 and lambda_two[mm]>-0.02:
+                        lambda_two[mm]=-0.02
 
 
-            
+
+
+
                 gt_matrix_noisy_two=gt_motion_rs_random_noisy(u_list,v_list,batch_size=input_parameters.batch_size,lambda_noisy=lambda_two)
                 gt_matrix_noisy_two=construct_matrix(gt_matrix_noisy_two,scale_factor=0.5,batch_size=input_parameters.batch_size)
 
 
-                
+
 
 
 
@@ -403,14 +411,14 @@ for current_epoch in range(input_parameters.epoch_num):
                 ssim_convex_two_left_further= -tf.math.minimum((ssim_shift_two_left_left+ssim_shift_two_left_right-2*ssim_shift_two_left)-(np.sum((0.5*lambda_two)**2)+np.sum((1.5*lambda_two)**2)-2*np.sum(lambda_two**2)),0)
                 '''
                 if input_parameters.dataset_name=='MSCOCO':
-                  ssim_convex_two_left_further=0
+                    ssim_convex_two_left_further=0
                 else:
 
-                  input_warped_to_template_shift_two_left_left=LK_layer_two.projective_inverse_warp(input_feature_map_two, gt_matrix_two+2.0*gt_matrix_noisy_two)
-                  ssim_shift_two_left_left=tf.reduce_mean(compute_ssim(template_feature_map_two,input_warped_to_template_shift_two_left_left))
+                    input_warped_to_template_shift_two_left_left=LK_layer_two.projective_inverse_warp(input_feature_map_two, gt_matrix_two+2.0*gt_matrix_noisy_two)
+                    ssim_shift_two_left_left=tf.reduce_mean(compute_ssim(template_feature_map_two,input_warped_to_template_shift_two_left_left))
 
-                 
-                  ssim_convex_two_left_further= -tf.math.minimum((ssim_shift_two_left_left-ssim_shift_two_left)-(np.sum((2*lambda_two)**2)-np.sum(lambda_two**2)),0)
+
+                    ssim_convex_two_left_further= -tf.math.minimum((ssim_shift_two_left_left-ssim_shift_two_left)-(np.sum((2*lambda_two)**2)-np.sum(lambda_two**2)),0)
 
 
 
@@ -429,17 +437,17 @@ for current_epoch in range(input_parameters.epoch_num):
                 ssim_convex_two_right_further= -tf.math.minimum((ssim_shift_two_right_left+ssim_shift_two_right_right-2*ssim_shift_two_right)-(np.sum((0.5*lambda_two)**2)+np.sum((1.5*lambda_two)**2)-2*np.sum(lambda_two**2)),0)
                 '''
                 if input_parameters.dataset_name=='MSCOCO':
-                  ssim_convex_two_right_further=0
+                    ssim_convex_two_right_further=0
                 else:
-                  input_warped_to_template_shift_two_right_right=LK_layer_two.projective_inverse_warp(input_feature_map_two, gt_matrix_two-2.0*gt_matrix_noisy_two)
-                  ssim_shift_two_right_right=tf.reduce_mean(compute_ssim(template_feature_map_two,input_warped_to_template_shift_two_right_right))
-
-                 
-                  ssim_convex_two_right_further= -tf.math.minimum((ssim_shift_two_right_right-ssim_shift_two_right)-(np.sum((2*lambda_two)**2)-np.sum(lambda_two**2)),0)
+                    input_warped_to_template_shift_two_right_right=LK_layer_two.projective_inverse_warp(input_feature_map_two, gt_matrix_two-2.0*gt_matrix_noisy_two)
+                    ssim_shift_two_right_right=tf.reduce_mean(compute_ssim(template_feature_map_two,input_warped_to_template_shift_two_right_right))
 
 
+                    ssim_convex_two_right_further= -tf.math.minimum((ssim_shift_two_right_right-ssim_shift_two_right)-(np.sum((2*lambda_two)**2)-np.sum(lambda_two**2)),0)
 
-                
+
+
+
 
                 if nn==0:
                     convex_loss=ssim_convex_two_left+ssim_convex_two_right+ssim_convex_two_left_further+ssim_convex_two_right_further
@@ -458,34 +466,30 @@ for current_epoch in range(input_parameters.epoch_num):
 
 
         all_parameters=level_two_template.trainable_variables+level_two_input.trainable_variables
-           
+
         grads = tape.gradient(total_loss, all_parameters)
         grads=[tf.clip_by_value(i,-0.1,0.1) for i in grads]
         optimizer.apply_gradients(zip(grads, all_parameters))
 
-      
 
- 
-        if iters%100==0 and iters>0:
-            
-            
-            print(iters)
-            print (save_path)
 
-            print (error_ave_1000/100)
-            print (ssim_loss_total/100)
-            print (convex_loss_total/100)
+
+        with summary_writer.as_default():
+            tf.summary.scalar('error_ave_1000', error_ave_1000, step=current_epoch)
+            tf.summary.scalar('ssim_loss_total', ssim_loss_total, step=current_epoch)
+            tf.summary.scalar('convex_loss_total', convex_loss_total, step=current_epoch)
+
             error_ave_1000=0.0
             convex_loss_total=0.0
             ssim_loss_total=0.0
 
-        if iters%input_parameters.save_eval_f==0 and iters>0:
+        # if iters%input_parameters.save_eval_f==0 and iters>0:
+        #
+        #     level_two_input.save_weights(save_path +'epoch_'+str(input_parameters.epoch_start+current_epoch)+"input_"+str(iters))
+        #     level_two_template.save_weights(save_path +'epoch_'+str(input_parameters.epoch_start+current_epoch)+"template_"+str(iters))
 
-            level_two_input.save_weights(save_path +'epoch_'+str(input_parameters.epoch_start+current_epoch)+"input_"+str(iters))
-            level_two_template.save_weights(save_path +'epoch_'+str(input_parameters.epoch_start+current_epoch)+"template_"+str(iters))
 
 
-           
         input_img = None
         u_list = None
         v_list = None
@@ -498,10 +502,3 @@ for current_epoch in range(input_parameters.epoch_num):
         input_warped_to_template_left_2=None
         input_warped_to_template_right_1=None
         input_warped_to_template_right_2=None
-
-
-       
-
-
-
-
